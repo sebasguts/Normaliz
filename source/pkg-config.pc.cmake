574f494f18e8c676f11f7861f6b39889446bde5d
Christof Soeger <csoeger@uos.de>
2015-10-02 15:59:16 +0200

    First try to create a pkg-config file libnormaliz.pc
    One useful information contained is the Cflags, which just gives all
    CXX_FLAGS. For a good version this should only be the necessary flags.
    Also Libs should be set proberly.

---
+++ source/pkg-config.pc.cmake
@@ -0,0 +1,9 @@
+Name: libnormaliz
+Description: library for computations with rational polyhedra and affine monoids
+Version: ${VERSION}
+Requires: ${PKG_CONFIG_REQUIRES}
+prefix=${prefix}
+includedir=${includedir}
+libdir=${libdir}
+Libs: ${PKG_CONFIG_LIBS}
+Cflags: ${PKG_CONFIG_CFLAGS}
